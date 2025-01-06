ebfd25d7d9d7c0069cf67d68bf971427944a0f73
Lucas. <iaminvisy@gmail.com>
2025-01-06 19:00:22 +0000

    guh the third

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv'
+dotenv.config()
 
 // https://astro.build/config
 export default defineConfig({
---
+++ src/scripts/gitrepo.js
@@ -0,0 +1,30 @@
+import { Octokit } from 'octokit';
+const API_KEY = import.meta.env.GITHUB_API_KEY;
+const commitTxt = document.querySelector('.info .commit a');
+
+const octokit = new Octokit({
+  auth: API_KEY,
+});
+
+async function logRepoDetails() {
+  try {
+    const res = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+      owner: 'invisyarcticfox',
+      repo: 'astrosite',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    });
+
+    const recentCommitHash = res.data[0]?.sha;
+    const recentCommitHashShort = recentCommitHash.slice(0, 7);
+    commitTxt.textContent = recentCommitHashShort || '#';
+    commitTxt.href =
+      `https://github.com/invisyarcticfox/astrosite/${recentCommitHash}` ||
+      '#';
+  } catch (error) {
+    console.error('Error fetching repository details:', error);
+  }
+}
+
+logRepoDetails();
