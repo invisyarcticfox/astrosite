8724f19889b651167e223eed5f983c86c4c944df
Lucas. <iaminvisy@gmail.com>
2025-02-18 15:49:08 +0000

    about

---
+++ src/scripts/age.js
@@ -0,0 +1,15 @@
+const elem = document.querySelector('b.age')
+function calculateAge(dobString) {
+  const dob = new Date(dobString);
+  const today = new Date();
+
+  let age = today.getFullYear() - dob.getFullYear();
+  const monthDifference = today.getMonth() - dob.getMonth();
+  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dob.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+const dob = "2005-05-04";
+elem.innerHTML = calculateAge(dob)
